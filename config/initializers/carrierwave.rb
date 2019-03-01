e003918d4e2cb8988c30f3c1efd8db061db02e94
ZoeBKramer <zoebkramer@gmail.com>
2019-03-01 04:32:47 +0000

    Fix config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
+
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl    = "public-read"
+  config.aws_acl    = "private"
 
                           
   config.aws_credentials =  {                     
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],       
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"], 
+    access_key_id:     ENV["AWS_ACCESS_KEY"],       
+    secret_access_key: ENV["AWS_SECRET_KEY"], 
     region:                ENV["AWS_REGION"]
   }
 end
